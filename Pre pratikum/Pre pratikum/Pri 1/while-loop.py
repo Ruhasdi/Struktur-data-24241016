e172f9cb7570028ddc2b98a9aa0fa334f303245d
Ruhasdi <ruhasdimusa2004@gmail.com>
2025-04-23 22:50:23 +0800

    Delete Pre pratikum/Pre pratikum/Pri 1 directory

--- Pre pratikum/Pre pratikum/Pri 1/while-loop.py
+++
@@ -1,9 +0,0 @@
-# Jumlah baris yang diinginkan
-n = 5
-
-# Perulangan untuk setiap baris
-for i in range(n):
-    # Menghitung jumlah bintang yang akan ditampilkan
-    # Hanya menampilkan bintang pada baris ganjil
-    if i % 2 == 0:  # Baris ganjil dalam indeks (0, 2, 4, ...)
-        print('*' * (2 * i + 1))  # Menampilkan bintang sesuai dengan baris
